779bb8849868b7d1dde9647174c029ffb6ab857b
Awais Hussain <awais.a.h2012@gmail.com>
2019-05-15 01:23:05 +0100

    Done up to step 4

--- src/TechTest/ClientApp/app/shared/colour-names-value-converter.ts
+++ src/TechTest/ClientApp/app/shared/colour-names-value-converter.ts
@@ -2,18 +2,32 @@ import { IColour } from '../people/interfaces/icolour';
 
 export class ColourNamesValueConverter {
 
-  toView(colours: IColour[]) {
+    toView(colours: IColour[]) {
+        // Implement the value converter function.
+        // Using the colours parameter, convert the list into a comma
+        // separated string of colour names. The names should be sorted
+        // alphabetically and there should not be a trailing comma.
+        //
+        // Example: 'Blue, Green, Red'
 
-    // TODO: Step 4
-    //
-    // Implement the value converter function.
-    // Using the colours parameter, convert the list into a comma
-    // separated string of colour names. The names should be sorted
-    // alphabetically and there should not be a trailing comma.
-    //
-    // Example: 'Blue, Green, Red'
+        /// Sort the values in the colours list of IColour
+        /// This is done alphabetically by comparing all values
+        colours.sort(function(a, b){
+            if(a.name < b.name) { return -1; }
+            if(a.name > b.name) { return 1; }
+            return 0;
+        })
 
-    return 'Todo';
-  }
+        /// Create an empty list of Strings
+        let coloursString:string[] = []; 
 
+        /// loop through all the values in colours
+        for (var colourIndex in colours) {
+            /// Push each colour name at the index currently being checked
+            coloursString.push(colours[colourIndex].name);
+        }
+
+        /// return the result of joining each element in the new coloursString list with ", "
+        return coloursString.join(", ");
+    }
 }
--- src/TechTest/Controllers/PeopleController.cs
+++ src/TechTest/Controllers/PeopleController.cs
@@ -19,34 +19,45 @@ namespace TechTest.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            // TODO: Step 1
-            //
             // Implement a JSON endpoint that returns the full list
             // of people from the PeopleRepository. If there are zero
             // people returned from PeopleRepository then an empty
             // JSON array should be returned.
 
-            throw new NotImplementedException();
+
+            //// Returns a new JsonResult with what is returned from PersonRepository
+            /// If nothing is returned an empty JSON array will be returned.
+
+            return new JsonResult(PersonRepository.GetAll());
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            // TODO: Step 2
-            //
             // Implement a JSON endpoint that returns a single person
             // from the PeopleRepository based on the id parameter.
             // If null is returned from the PeopleRepository with
             // the supplied id then a NotFound should be returned.
 
-            throw new NotImplementedException();
+            //// Get a Person istance from PersonRepository with Id id
+            Person p = PersonRepository.Get(id);
+
+            //// Check if the Person p is not null 
+            if (p != null)
+            {
+                //// If p is not null the return the Json instance of p
+                return new JsonResult(PersonRepository.Get(id));
+            }
+
+            //// only reach this point if p is null
+            return NotFound();
+
+
         }
 
         [HttpPut("{id}")]
         public IActionResult Update(int id, PersonUpdate personUpdate)
         {
-            // TODO: Step 3
-            //
             // Implement an endpoint that receives a JSON object to
             // update a person using the PeopleRepository based on
             // the id parameter. Once the person has been successfully
@@ -54,7 +65,37 @@ namespace TechTest.Controllers
             // If null is returned from the PeopleRepository then a
             // NotFound should be returned.
 
-            throw new NotImplementedException();
+            System.Diagnostics.Debug.WriteLine(personUpdate.ToString());
+
+            //// Get a Person istance from PersonRepository with Id id
+            Person p = PersonRepository.Get(id);
+
+            //// Check if the Person p is null 
+            if (p == null)
+            {
+                //// If p is null the return NotFound
+                return NotFound();
+            }
+
+
+            //// If not null will update p's values for Authorised, Enabled and Colours
+            /// with those from personUpdaye
+            p.Authorised = personUpdate.Authorised;
+            p.Enabled = personUpdate.Enabled;
+            p.Colours = personUpdate.Colours;
+
+            //// Get a Person istance from PersonRepository when doing Update
+            p = PersonRepository.Update(p);
+
+            //// Check if the Person p is not null 
+            if (p != null)
+            {
+                //// If p is not null the return the Json instance of p
+                return new JsonResult(p);
+            }
+
+            //// Otherwise return NotFound
+            return NotFound();
         }
     }
 }
